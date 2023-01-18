f8d4bcea23465bd0dbd4c09294dca2a0fcdb01b6
Garand Tyson <garand@stellar.org>
2023-01-18 15:23:20 -0800

    Sketch untested V0 (#5)
    * Sketch untested V0
    
    * Update lib.rs
    
    Added set owner and set resolver based on existing v0 node creation pattern
    
    * Fix compilation errors
    
    * Added initialization test
    
    * Working with basic tests
    
    Co-authored-by: Nick Gilbert <nick.gilbert@appdirect.com>
    Co-authored-by: Gleb <gleb@stellar.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,207 @@
 #![no_std]
 
-use soroban_sdk::{contractimpl, contracttype, Env, map, Map, BytesN, Address, Bytes};
+use soroban_sdk::{
+    contracterror, contractimpl, contracttype, map, Address, Bytes, BytesN, Env, Map,
+};
 
 pub struct Contract;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    // Resolution errors
+    InvalidHashInput = 1,
+    NotFound = 2,
+    // Registration errors
+    ParentNotFound = 3,
+    NotAuthorized = 4,
+}
 
 #[derive(Clone)]
 #[contracttype]
 pub enum DataKey {
-    RMap
+    RMap,
 }
 
 #[contracttype]
 pub struct Node {
     pub owner: Address,
     pub p_hash: BytesN<32>,
-    pub res_addr: Address
+    pub res_addr: Address,
+}
+
+fn map_get(env: &Env, key: BytesN<32>) -> Option<Node> {
+    let map: Map<BytesN<32>, Node> = env.storage().get_unchecked(DataKey::RMap).unwrap();
+    match map.get(key) {
+        Some(res) => match res {
+            Ok(node) => Some(node),
+            _ => None,
+        },
+        None => None,
+    }
+}
+
+fn map_insert(env: &Env, key: BytesN<32>, node: Node) {
+    let mut map: Map<BytesN<32>, Node> = env.storage().get_unchecked(DataKey::RMap).unwrap();
+    map.set(key, node);
+    env.storage().set(DataKey::RMap, map);
+}
+
+fn empty_hash(env: &Env) -> BytesN<32> {
+    BytesN::from_array(&env, &[0; 32])
+}
+
+// Checks if caller owns node or any of node's parents
+fn auth_check(env: &Env, node: &Node) -> bool {
+    if node.owner == env.invoker() {
+        return true;
+    }
+
+    // If parent hash is empty, current node mut be root node
+    let parent_hash = node.p_hash.clone();
+    if parent_hash.is_empty() {
+        return false;
+    }
+
+    let parent_node = map_get(env, parent_hash).unwrap();
+    auth_check(env, &parent_node)
+}
+
+fn append_hash(env: &Env, parent_hash: &BytesN<32>, leaf_hash: &BytesN<32>) -> BytesN<32> {
+    let mut bytes = Bytes::new(env);
+    bytes.append(&leaf_hash.clone().into());
+    bytes.append(&parent_hash.clone().into());
+    env.crypto().sha256(&bytes)
 }
 
 #[contractimpl]
 impl Contract {
-
-    fn append_hash(env: &Env, parent_hash: &BytesN<32>, leaf_hash: &BytesN<32>) -> BytesN<32> {
-        let mut bytes = Bytes::new(env);
-        bytes.append(&leaf_hash.clone().into());
-        bytes.append(&parent_hash.clone().into());
-        env.crypto().sha256(&bytes)
-    }
-
     pub fn init(env: Env) {
         if env.storage().has(DataKey::RMap) {
             panic!("Contract already initialized")
         }
 
-        let map: Map<BytesN<32>, Node> = map![&env];
+        let mut map: Map<BytesN<32>, Node> = map![&env];
 
-        env.storage().set(DataKey::RMap, map)
+        // Root node is empty hash, owned by contract initializer
+        map.set(
+            empty_hash(&env),
+            Node {
+                owner: env.invoker(),
+                p_hash: empty_hash(&env),
+                res_addr: env.invoker(), // This should be empty but I don't know how to default init Address
+            },
+        );
+
+        env.storage().set(DataKey::RMap, map);
     }
 
-    pub fn get_map(env: Env) -> Map<BytesN<32>, Node> {
-        return env.storage().get_unchecked(DataKey::RMap).unwrap()
+    // Given a nameHash, returns the associated address
+    pub fn resolve(env: Env, hash: BytesN<32>) -> Result<Address, Error> {
+        // Should not support empty queries, even if "technically" possible with initial root node
+        if hash.is_empty() {
+            return Err(Error::InvalidHashInput);
+        }
+
+        match map_get(&env, hash) {
+            Some(node) => Ok(node.res_addr),
+            None => Err(Error::NotFound),
+        }
+    }
+
+    // Registers subdomain under parent node
+    pub fn register(
+        env: Env,
+        parent_hash: BytesN<32>,
+        leaf_hash: BytesN<32>,
+        owner: Address,
+        res_addr: Address,
+    ) -> Result<BytesN<32>, Error> {
+        // Check if parent hash exists
+        let parent_node = match map_get(&env, parent_hash.clone()) {
+            Some(node) => node,
+            None => return Err(Error::ParentNotFound),
+        };
+
+        // Check if invoker is authorized to create subdomain
+        if !auth_check(&env, &parent_node) {
+            return Err(Error::NotAuthorized);
+        }
+
+        // Insert new node
+        let key = append_hash(&env, &parent_hash, &leaf_hash);
+        map_insert(
+            &env,
+            key.clone(),
+            Node {
+                owner: owner,
+                p_hash: parent_hash,
+                res_addr,
+            },
+        );
+
+        Ok(key)
+    }
+
+    pub fn set_owner(env: Env, hash: BytesN<32>, new_owner: Address) -> Result<BytesN<32>, Error> {
+        // Check if hash exists
+        let node = match map_get(&env, hash.clone()) {
+            Some(res) => res,
+            None => return Err(Error::InvalidHashInput),
+        };
+
+        // Check if invoker is authorized to edit the owner
+        if !auth_check(&env, &node) {
+            return Err(Error::NotAuthorized);
+        }
+
+        map_insert(
+            &env,
+            hash.clone(),
+            Node {
+                owner: new_owner,
+                p_hash: node.p_hash,
+                res_addr: node.res_addr,
+            },
+        );
+
+        Ok(hash.clone())
+    }
+
+    pub fn set_res(env: Env, hash: BytesN<32>, new_resolv: Address) -> Result<BytesN<32>, Error> {
+        // Check if hash exists
+        let node = match map_get(&env, hash.clone()) {
+            Some(res) => res,
+            None => return Err(Error::InvalidHashInput),
+        };
+
+        // Check if invoker is authorized to edit the resolver
+        if !auth_check(&env, &node) {
+            return Err(Error::NotAuthorized);
+        }
+
+        map_insert(
+            &env,
+            hash.clone(),
+            Node {
+                owner: node.owner,
+                p_hash: node.p_hash,
+                res_addr: new_resolv,
+            },
+        );
+
+        Ok(hash.clone())
+    }
+
+    #[cfg(test)]
+    pub fn t_get(env: Env, key: BytesN<32>) -> Option<Node> {
+        map_get(&env, key)
+    }
+
+    #[cfg(test)]
+    pub fn t_insert(env: Env, key: BytesN<32>, node: Node) {
+        map_insert(&env, key, node)
     }
 }
 
