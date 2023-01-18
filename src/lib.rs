ffe8e7df82662ba3a1131141428ea6ee43f82427
Gleb <gleb@stellar.org>
2023-01-17 17:21:33 -0800

    Add contract initialization (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,36 @@
 #![no_std]
-use soroban_sdk::{contractimpl, symbol, vec, Env, Symbol, Vec};
+
+use soroban_sdk::{contractimpl, contracttype, symbol, vec, Env, Symbol, Vec, map, Map, BytesN, Address};
 
 pub struct Contract;
 
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    RMap
+}
+
+#[contracttype]
+pub struct Node {
+    pub owner: Address,
+    pub p_hash: BytesN<32>,
+    pub res_addr: Address
+}
+
 #[contractimpl]
 impl Contract {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        Vec::from_array(&env, [symbol!("Hello"), to])
+    pub fn init(env: Env) {
+        if env.storage().has(DataKey::RMap) {
+            panic!("Contract already initialized")
+        }
+
+        let map: Map<BytesN<32>, Node> = map![&env];
+
+        env.storage().set(DataKey::RMap, map)
+    }
+
+    pub fn get_map(env: Env) -> Map<BytesN<32>, Node> {
+        return env.storage().get_unchecked(DataKey::RMap).unwrap()
     }
 }
 
@@ -16,16 +40,14 @@ mod test {
     use soroban_sdk::{symbol, vec, Env};
 
     #[test]
-    fn test() {
+    fn init_not_throws() {
         let env = Env::default();
         let contract_id = env.register_contract(None, Contract);
         let client = ContractClient::new(&env, &contract_id);
 
-        let words = client.hello(&symbol!("Dev"));
-        assert_eq!(
-            words,
-            vec![&env, symbol!("Hello"), symbol!("Dev"),]
-        );
+        client.init();
+
+        let map = client.get_map();
     }
 }
 
