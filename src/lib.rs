98552996536c74a8b4b567a63383b5803baad4ea
Nick Gilbert <nick.gilbert@appdirect.com>
2023-01-17 20:26:58 -0800

    functions to be added
    list of functions which need to be added.

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,33 @@ impl Contract {
     pub fn get_map(env: Env) -> Map<BytesN<32>, Node> {
         return env.storage().get_unchecked(DataKey::RMap).unwrap()
     }
+       /* 
+    function to check it the invoker is authorzed to act on a record
+    fn auth_check(node) -> Boolean {
+        check invoker == node.owner
+        else tree_check (node.p_hash) 
+    }
+    function to trace p_hash back to root looking for authorization - Need a special case for p_hash on the root to end loop.
+    fn tree_check(node) -> Boolean {
+        while node.p_hash != <special case> 
+    }
+    function to read the owner of a record
+    pub fn owner(node) -> address{
+
+    }
+    
+    function to read the resolver of a record
+    pub fn resolver(node) -> address{
+
+    }
+    function to create a new record args: parent hash, label hash, owner of record to be created, resolver for record to be created. 
+    pub fn createNode(p_hash,leaf_hash,owner,resolver) {
+       if auth_check (p_hash) 
+       then create new map entry with label appned_hash(p_hash,leaf_hash) and owner = owner and resolver = resolver
+    }
+
+
+    */
 }
 
 mod test;
