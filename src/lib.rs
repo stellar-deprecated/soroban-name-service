e93665c9acc46c11239e93bcf735fa324a3e56fe
Garand Tyson <garand@stellar.org>
2023-01-18 11:51:36 -0800

    Added initialization test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
-use soroban_sdk::{contractimpl, contracttype, contracterror, Env, map, Map, BytesN, Address, Bytes};
+use soroban_sdk::{
+    contracterror, contractimpl, contracttype, map, Address, Bytes, BytesN, Env, Map,
+};
 
 pub struct Contract;
 
@@ -19,7 +21,7 @@ pub enum Error {
 #[derive(Clone)]
 #[contracttype]
 pub enum DataKey {
-    RMap
+    RMap,
 }
 
 #[contracttype]
@@ -27,6 +29,34 @@ pub struct Node {
     pub owner: Address,
     pub p_hash: BytesN<32>,
     pub res_addr: Address,
+}
+
+fn get_map(env: &Env) -> Map<BytesN<32>, Node> {
+    return env.storage().get_unchecked(DataKey::RMap).unwrap();
+}
+
+// Checks if caller owns node or any of node's parents
+fn auth_check(env: &Env, node: &Node) -> bool {
+    if node.owner == env.invoker() {
+        return true;
+    }
+
+    // If parent hash is empty, current node mut be root node
+    let parent_hash = node.p_hash.clone();
+    if parent_hash.is_empty() {
+        return false;
+    }
+
+    let map = get_map(env);
+    let parent_node = map.get(parent_hash).unwrap().unwrap();
+    auth_check(env, &parent_node)
+}
+
+fn append_hash(env: &Env, parent_hash: &BytesN<32>, leaf_hash: &BytesN<32>) -> BytesN<32> {
+    let mut bytes = Bytes::new(env);
+    bytes.append(&leaf_hash.clone().into());
+    bytes.append(&parent_hash.clone().into());
+    env.crypto().sha256(&bytes)
 }
 
 #[contractimpl]
@@ -39,11 +69,14 @@ impl Contract {
         let mut map: Map<BytesN<32>, Node> = map![&env];
 
         // Root node is empty hash, owned by contract initializer
-        map.set(BytesN::from_array(&env, &[0; 32]), Node {
-            owner: env.invoker(),
-            p_hash: BytesN::from_array(&env, &[0; 32]),
-            res_addr: env.invoker(), // This should be empty but I don't know how to default init Address
-        });
+        map.set(
+            BytesN::from_array(&env, &[0; 32]),
+            Node {
+                owner: env.invoker(),
+                p_hash: BytesN::from_array(&env, &[0; 32]),
+                res_addr: env.invoker(), // This should be empty but I don't know how to default init Address
+            },
+        );
 
         env.storage().set(DataKey::RMap, map)
     }
@@ -55,111 +88,102 @@ impl Contract {
             return Err(Error::InvalidHashInput);
         }
 
-        let map = Self::get_map(&env);
+        let map = get_map(&env);
         match map.get(hash) {
             Some(node) => Ok(node.unwrap().res_addr),
-            None => Err(Error::NotFound)
+            None => Err(Error::NotFound),
         }
     }
 
+    pub fn map_test(env: Env) -> Map<BytesN<32>, Node> {
+        get_map(&env)
+    }
+
     // Registers subdomain under parent node
-    pub fn register(env: Env, parent_hash: BytesN<32>, leaf_hash: BytesN<32>, owner: Address, res_addr: Address) -> Result<BytesN<32>, Error> {
-        let mut map = Self::get_map(&env);
+    pub fn register(
+        env: Env,
+        parent_hash: BytesN<32>,
+        leaf_hash: BytesN<32>,
+        owner: Address,
+        res_addr: Address,
+    ) -> Result<BytesN<32>, Error> {
+        let mut map = get_map(&env);
 
         // Check if parent hash exists
         let parent_node = match map.get(parent_hash.clone()) {
             Some(node) => node.unwrap(),
-            None => return Err(Error::ParentNotFound)
+            None => return Err(Error::ParentNotFound),
         };
 
         // Check if invoker is authorized to create subdomain
-        if !Self::auth_check(&env, &parent_node) {
+        if !auth_check(&env, &parent_node) {
             return Err(Error::NotAuthorized);
         }
 
         // Insert new node
-        let key = Self::append_hash(&env, &parent_hash, &leaf_hash);
-        map.set(key.clone(), Node {
-            owner: owner,
-            p_hash: parent_hash,
-            res_addr,
-        });
+        let key = append_hash(&env, &parent_hash, &leaf_hash);
+        map.set(
+            key.clone(),
+            Node {
+                owner: owner,
+                p_hash: parent_hash,
+                res_addr,
+            },
+        );
 
         Ok(key)
     }
 
-    pub fn setOwner(env: Env, hash: BytesN<32>, new_owner: Address) -> Result<BytesN<32>, Error> {
-        let mut map = Self::get_map(&env);
+    pub fn set_owner(env: Env, hash: BytesN<32>, new_owner: Address) -> Result<BytesN<32>, Error> {
+        let mut map = get_map(&env);
 
         // Check if hash exists
         let node = match map.get(hash.clone()) {
             Some(res) => res.unwrap(),
-            None => return Err(Error::InvalidHashInput)
+            None => return Err(Error::InvalidHashInput),
         };
 
         // Check if invoker is authorized to edit the owner
-        if !Self::auth_check(&env, &node) {
+        if !auth_check(&env, &node) {
             return Err(Error::NotAuthorized);
         }
 
-        map.set(hash.clone(), Node {
-            owner: new_owner,
-            p_hash: node.p_hash,
-            res_addr: node.res_addr,
-        });
+        map.set(
+            hash.clone(),
+            Node {
+                owner: new_owner,
+                p_hash: node.p_hash,
+                res_addr: node.res_addr,
+            },
+        );
 
         Ok(hash.clone())
     }
 
-    pub fn setRes(env: Env, hash: BytesN<32>, new_resolv: Address) -> Result<BytesN<32>, Error> {
-        let mut map = Self::get_map(&env);
+    pub fn set_res(env: Env, hash: BytesN<32>, new_resolv: Address) -> Result<BytesN<32>, Error> {
+        let mut map = get_map(&env);
 
         // Check if hash exists
         let node = match map.get(hash.clone()) {
             Some(res) => res.unwrap(),
-            None => return Err(Error::InvalidHashInput)
+            None => return Err(Error::InvalidHashInput),
         };
 
         // Check if invoker is authorized to edit the resolver
-        if !Self::auth_check(&env, &node) {
+        if !auth_check(&env, &node) {
             return Err(Error::NotAuthorized);
         }
 
-        map.set(hash.clone(), Node {
-            owner: node.owner,
-            p_hash: node.p_hash,
-            res_addr: new_resolv,
-        });
+        map.set(
+            hash.clone(),
+            Node {
+                owner: node.owner,
+                p_hash: node.p_hash,
+                res_addr: new_resolv,
+            },
+        );
 
         Ok(hash.clone())
-    }
-
-    // Checks if caller owns node or any of node's parents
-    fn auth_check(env: &Env, node: &Node) -> bool {
-        if node.owner == env.invoker() {
-            return true;
-        }
-
-        // If parent hash is empty, current node mut be root node
-        let parent_hash = node.p_hash.clone();
-        if parent_hash.is_empty() {
-            return false;
-        }
-
-        let map = Self::get_map(env);
-        let parent_node = map.get(parent_hash).unwrap().unwrap();
-        Self::auth_check(env, &parent_node)
-    }
-
-    fn append_hash(env: &Env, parent_hash: &BytesN<32>, leaf_hash: &BytesN<32>) -> BytesN<32> {
-        let mut bytes = Bytes::new(env);
-        bytes.append(&leaf_hash.clone().into());
-        bytes.append(&parent_hash.clone().into());
-        env.crypto().sha256(&bytes)
-    }
-
-    fn get_map(env: &Env) -> Map<BytesN<32>, Node> {
-        return env.storage().get_unchecked(DataKey::RMap).unwrap();
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,24 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{Env};
+use soroban_sdk::{testutils::Accounts, Env};
 
-    #[test]
-    fn init_not_throws() {
-        let env = Env::default();
-        let contract_id = env.register_contract(None, Contract);
-        let client = ContractClient::new(&env, &contract_id);
+#[test]
+fn init_not_throws() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Contract);
+    let client = ContractClient::new(&env, &contract_id);
 
-        client.init();
+    let admin = env.accounts().generate();
+    client.with_source_account(&admin).init();
 
-        let _ = client.get_map();
-    }
+    let map = client.map_test();
+    let root_node = map
+        .get(BytesN::from_array(&env, &[0; 32]))
+        .unwrap()
+        .unwrap();
+
+    // Check root node defaults
+    assert_eq!(root_node.owner, Address::Account(admin));
+    assert_eq!(root_node.p_hash, BytesN::from_array(&env, &[0; 32]));
+}
